2acbf6aeefbbc3210a4ddd025a54a69a6b45559f
Mxn-ptr <maxence0@hotmail.fr>
2022-03-02 17:03:54 +0100

    Add do_create, do_show, do_destroy

--- console.py
+++ console.py
@@ -1,55 +1,83 @@
 #!/usr/bin/python3
-import json
+"""Defines the command interpreter"""
 import cmd
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
+    """Define the command interpreter"""
+    
     prompt = '(hbnb) '
     file = None
+    __classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "Review": Review
+    }
 
     def do_quit(self, arg):
         'Quit command to exit the program'
-        self.close()
-        quit()
         return True
 
     def do_EOF(self, arg):
         'Quit command to exit the program'
-        self.close()
-        quit()
         return True
 
     def do_create(self, arg):
         'Create command to create a new instance'
-        if str(*parse(arg)) == "":
+        if arg == "":
             print("** class name missing **")
-        elif str(*parse(arg)) == "BaseModel":
-            bm = BaseModel()
-            print(bm.id)
+        elif arg in self.__classes:
+            a = self.__classes[arg]()
+            storage.save()
+            print(a.id)
         else:
             print("** class doesn't exist **")
-
-    def do_all
    
     def do_show(self, arg):
-	a = (parse(arg)[1])
-        b = (parse(arg)[2])
-        with open("file.json", "r") as f:
-             list_obj = json.load(f)
-             for k in list_obj:
-                 if k ==
+        words = arg.split()
+        if arg == "":
+            print("** class name missing **")
+        elif words[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(words) < 2:
+            print("** instance is missing **")
+        else:
+            inst = "{}.{}".format(words[0], words[1])
+            if inst not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[inst])
 
-    def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
+    def do_destroy(self, arg):
+        words = arg.split()
+        if arg == "":
+            print("** class name missing **")
+        elif words[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(words) < 2:
+            print("** instance is missing **")
+        else:
+            inst = "{}.{}".format(words[0], words[1])
+            if inst not in storage.all():
+                print("** no instance found **")
+            else:
+                del storage.all()[inst]
+                storage.save()
 
     def emptyline(self):
         pass
-
-def parse(arg):
-    'Convert argulents to an argument tuple'
-    return tuple(map(str, arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-a = BaseModel()
-print(a.to_dict())
