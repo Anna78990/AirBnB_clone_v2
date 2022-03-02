eb6c8673c6eb011671a576f47df4d231b6ce89d7
Mxn-ptr <maxence0@hotmail.fr>
2022-03-02 15:27:40 +0100

    Fix importation

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define a class Amenity"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define a class State"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
