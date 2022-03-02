b3ac2e5f2ce6696827307a2a0d5f96ccb15a6d60
Anna78990 <takasuanna1990@gmail.com>
2022-03-02 14:47:14 +0000

    do all do show

--- console.py
+++ console.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Defines a command interpreter"""
+import json
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
-    """Represents the command interpreter"""
     prompt = '(hbnb) '
     file = None
 
@@ -20,16 +19,37 @@ class HBNBCommand(cmd.Cmd):
         quit()
         return True
 
+    def do_create(self, arg):
+        'Create command to create a new instance'
+        if str(*parse(arg)) == "":
+            print("** class name missing **")
+        elif str(*parse(arg)) == "BaseModel":
+            bm = BaseModel()
+            print(bm.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_all
+   
+    def do_show(self, arg):
+	a = (parse(arg)[1])
+        b = (parse(arg)[2])
+        with open("file.json", "r") as f:
+             list_obj = json.load(f)
+             for k in list_obj:
+                 if k ==
+
     def close(self):
-        """Close command"""
         if self.file:
             self.file.close()
             self.file = None
 
     def emptyline(self):
-        """Empty line do nothing"""
         pass
 
+def parse(arg):
+    'Convert argulents to an argument tuple'
+    return tuple(map(str, arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
