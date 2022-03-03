3909dbc5bcc9a6019f119cc1c284f268dca86085
Mxn-ptr <maxence0@hotmail.fr>
2022-03-03 12:13:03 +0100

    Add do_all and do_update

--- console.py
+++ console.py
@@ -78,6 +78,55 @@ class HBNBCommand(cmd.Cmd):
                 del storage.all()[inst]
                 storage.save()
 
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+        words = arg.split()
+        if len(arg) > 0 and words[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            list_obj = []
+            for obj in storage.all().values():
+                if len(arg) > 0 and words[0] == obj.__class__.__name__:
+                    list_obj.append(obj.__str__())
+                elif len(arg) == 0:
+                    list_obj.append(obj.__str__())
+            print(list_obj)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name
+        and id by adding or updating attribute"""
+        words = arg.split()
+        cls_name = words[0] if len(words) > 0 else None
+        id = words[1] if len(words) > 1 else None
+        attribute = words[2] if len(words) > 2 else None
+        value = words[3] if len(words) > 3 else None
+        obj = "{}.{}".format(cls_name, id)
+        if cls_name is None:
+            print("** class name missing **")
+            return False
+        if cls_name not in self.__classes:
+            print("** class doesn't exist **")
+            return False
+        if id is None:
+            print("** instance id missing **")
+            return False
+        if obj not in storage.all().keys():
+            print("** no instance found **")
+            return False
+        if attribute is None:
+            print("** attribute name missing **")
+            return False
+        if len(words) == 4:
+            if words[2] not in self.__classes[cls_name].__dict__.keys():
+                print("** value missing **")
+            else:
+                if type(value) == str:
+                    value = '{}'.format(value[1:-1])
+                setattr(storage.all()[obj], attribute, words[3])
+                storage.save()
+            
+
     def emptyline(self):
         """Do nothing when emptyline is entered"""
         pass
