63c0b62ffee7281b35c57c64b8542644dda6fd72
Anna78990 <takasuanna1990@gmail.com>
2022-03-04 15:48:20 +0000

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,13 +7,13 @@ import time
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
-    """ Test of class Base """
+    """ Test of class BaseModel """
 
     def test_id(self):
         """test for id attribute of BaseModel
         """
         b = BaseModel()
-        self.assertEqual(type(b.id), type("abc"))
+        self.assertEqual(type(b.id), type("a"))
 
     def test_created_at(self):
         """test for created_at attribute of BaseModel
@@ -128,6 +128,14 @@ class TestBaseModel(unittest.TestCase):
         bdic = b.to_dict()
         a = BaseModel(**bdic)
         self.assertEqual(a.id, b.id)
+
+    def test_init_kwargs2(self):
+        """test for method __init__ of BaseModel by kwagrs
+        """
+        b = BaseModel()
+        bdic = b.to_dict()
+        a = BaseModel(**bdic)
+        self.assertEqual(a.created_at, b.created_at)
 
 if __name__ == "__main__":
     unittest.main()
