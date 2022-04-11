4bff048fe7dfe2acbcb08c9452254242b7ab842f
Mxn-ptr <maxence0@hotmail.fr>
2022-04-11 09:52:55 +0200

    Fix task 3 and 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,3 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL ON DATABASE hbnb_test_db TO 'hbnb_test'@'localhost';
 GRANT SELECT ON DATABASE performance_schema TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
