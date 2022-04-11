eb886871459ce9fb344b5649a7048f76b7c88f2b
Mxn-ptr <maxence0@hotmail.fr>
2022-04-11 10:29:58 +0200

    Fix task 3 and 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,13 @@
--- This script will create the database --
--- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema --
+-- This script will create the database
+-- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON DATABASE hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON DATABASE performance_schema TO 'hbnb_dev'@'localhost';
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+	ON `hbnb_dev_db`.*
+	TO 'hbnb_dev'@'localhost';
+GRANT SELECT
+	ON `performance_schema`.*
+	TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,13 @@
 -- This script will create the database (this is the test) --
 -- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON DATABASE hbnb_test_db TO 'hbnb_test'@'localhost';
-GRANT SELECT ON DATABASE performance_schema TO 'hbnb_test'@'localhost';
+CREATE USER
+	IF NOT EXISTS 'hbnb_test'@'localhost'
+	IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL
+	ON `hbnb_test_db`.*
+	TO 'hbnb_test'@'localhost';
+GRANT SELECT 
+	ON `performance_schema`.*
+	TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
