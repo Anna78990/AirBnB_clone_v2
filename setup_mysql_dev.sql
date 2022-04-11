40d561d8fc6b47c30648ca7d253df6f7728e279e
NeoV0id <neov0id.nova@gmail.com>
2022-04-11 00:33:47 -0700

    Modified: added keyword 'Database'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,5 @@
 -- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema --
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT * ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT * ON DATABASE hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON DATABASE performance_schema TO 'hbnb_dev'@'localhost';
