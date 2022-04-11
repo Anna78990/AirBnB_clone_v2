d42d38311f3c6e9f1123ffbeb85ba62a9acf66d4
NeoV0id <neov0id.nova@gmail.com>
2022-04-11 00:28:31 -0700

    Fix setup files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- This script will create the database (this is the test) --
 -- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema --
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT * ON 'hbnb_test_db' TO 'hbnb_test'@'localhost';
-GRANT SELECT ON 'performance_schema' TO 'hbnb_test'@'localhost';
+GRANT * ON hbnb_test_db TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
