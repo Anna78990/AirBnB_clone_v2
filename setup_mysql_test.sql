ea75c93c156519a83fff310085e00448f5176a4b
NeoV0id <neov0id.nova@gmail.com>
2022-04-11 00:40:47 -0700

    Added keyword ALL to GRANT on setup files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,5 @@
 -- hbnb_dev_db should have all privileges on hbnb_dev_db and SELECT on performance schema --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT * ON hbnb_test_db TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT ALL ON DATABASE hbnb_test_db TO 'hbnb_test'@'localhost';
+GRANT SELECT ON DATABASE performance_schema TO 'hbnb_test'@'localhost';
