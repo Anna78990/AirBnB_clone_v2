77930c867f8024b787377f7782717e6195ff1a52
Mxn-ptr <maxence0@hotmail.fr>
2022-04-12 11:44:53 +0200

    Fix

--- models/place.py
+++ models/place.py
@@ -24,15 +24,15 @@ class Place(BaseModel, Base):
         - amenity_ids (list): list of amenity's ids
         """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities_id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
 
