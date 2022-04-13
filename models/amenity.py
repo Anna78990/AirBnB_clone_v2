d2394e27b099bcacff28ca345b87eaed88df9104
Mxn-ptr <maxence0@hotmail.fr>
2022-04-13 17:48:36 +0200

    Try to fix task 6

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,9 @@
 """Define a class Amenity"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
-
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Represent an amenity
 
     Attribute:
@@ -16,4 +15,4 @@ class Amenity(BaseModel):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", back_populates="amenities")
+    place_amenities = relationship("Place", secondary="place_amenity")
