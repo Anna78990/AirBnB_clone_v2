4a65d9bd900ab6b32cac3eb2f563bd21b1206b50
Anna78990 <takasuanna1990@gmail.com>
2022-04-14 20:51:32 +0000

    console.py

---
+++ tests/test_console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Unittest for class City """
+import unittest
+from datetime import datetime
+import time
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+console = __import__('console').HBNBCommand
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Test of console
+    """
+
+    def test_console(self):
+        """
+        testing name type for class City
+        """
+        con = console()
+        arg = 'State name="Arizona"'
+        with open('file.json') as f:
+            contents = f.read()
+        f.close()
+        bool = '"name": "Arizona"' in contents
+        message = "Test value is not true."
+        self.assertTrue(bool, message)
+
+
+if __name__ == "__main__":
+    unittest.main()
