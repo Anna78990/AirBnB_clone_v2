0e7ddb788266b6e5fd5dba867bb91051605faa7f
Anna78990 <takasuanna1990@gmail.com>
2022-04-15 08:06:46 +0000

    testplace

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -16,14 +16,6 @@ class TestPlace(unittest.TestCase):
         p = Place()
         self.assertEqual(type(p.id), type("a"))
 
-    def test_created_at(self):
-        """test for created_at attribute of Place
-        """
-        p = Place()
-        d = datetime.now()
-        dst = d.strftime('%Y-%m-%d %H:%M')
-        self.assertEqual(str(p.created_at)[0:-10], dst)
-
     def test_updated_at(self):
         """test for updated_at attribute of Place
         """
@@ -37,12 +29,6 @@ class TestPlace(unittest.TestCase):
         """
         p = Place(city_id="a")
         self.assertEqual(p.city_id, "a")
-
-    def test_city_id2(self):
-        """test for city_id attribute of Place
-        """
-        p = Place()
-        self.assertEqual(p.city_id, "")
 
     def test_user_id(self):
         """test for user_id attribute of Place
@@ -73,12 +59,6 @@ class TestPlace(unittest.TestCase):
         """
         p = Place(description="test")
         self.assertEqual(p.description, "test")
-
-    def test_description2(self):
-        """test for description attribute of Place
-        """
-        p = Place()
-        self.assertEqual(p.description, "")
 
     def test_number_rooms(self):
         """test for number_rooms attribute of Place
@@ -134,12 +114,6 @@ class TestPlace(unittest.TestCase):
         p = Place(latitude=1)
         self.assertEqual(p.latitude, 1.0)
 
-    def test_latitude2(self):
-        """test for latitude attribute of Place
-        """
-        p = Place()
-        self.assertEqual(p.latitude, 0.0)
-
     def test_longitude(self):
         """test for longitude attribute of Place
         """
@@ -157,12 +131,6 @@ class TestPlace(unittest.TestCase):
         """
         p = Place()
         self.assertEqual(p.amenity_ids, "")
-
-    def amenity_ids2(self):
-        """test for amenity_ids attribute of Place
-        """
-        p = Place(amenity_ids="a1")
-        self.assertEqual(p.amenity_ids, "a1")
 
     def test_to_dict(self):
         """test for method to_dict of Place
