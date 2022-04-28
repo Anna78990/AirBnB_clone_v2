3519ff9612f8f457a9a9bb8ad3d6b753de341dd9
Anna78990 <takasuanna1990@gmail.com>
2022-04-28 12:16:12 +0000

    install nginx if not exists

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #sets up the web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
