dcc1b3cdd9ba951543ff65841a85ad4a6bf4f821
Anna78990 <takasuanna1990@gmail.com>
2022-05-15 12:55:07 +0000

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,56 +1,47 @@
-from flask import Flask, abort, render_template
+#!/usr/bin/python3
 """ practice of flask """
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-""" print hello world """
 @app.route('/', strict_slashes=False)
 def index():
+    """ print hello world """
     return "Hello HBNB!"
 
 
-""" print hello HBNB """
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ print HBNB """
     return "HBNB"
 
 
-""" print the path after /c/ """
 @app.route('/', defaults={'path': ''})
 @app.route('/c/<path:path>', strict_slashes=False)
 def show_your_text(path):
+    """ print C is 'path' """
     path = path.replace('_', ' ')
     return 'C %s' % path
 
 
-""" print the path after /Python/, if it is empty, print 'is cool'"""
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<path>', strict_slashes=False)
 def show_your_text_python(path="is_cool"):
+    """ print Python is 'path' """
     path = path.replace('_', ' ')
     return 'Python %s' % path
 
 
-""" print the path after /number/ if it is number """
-@app.route('/number', strict_slashes=False)
-@app.route('/number/<path>', strict_slashes=False)
+@app.route('/number/<int:path>', strict_slashes=False)
 def show_your_text_number(path):
-    if path.isdigit():
-        return '{} is a number'.format(path)
-    else:
-        abort(404)
+    """ print 'path' is a number """
+    return '{} is a number'.format(path)
 
 
-""" print the path after /number_template/ if it is number """
-@app.route('/number_template', strict_slashes=False)
-@app.route('/number_template/<path>', strict_slashes=False)
+@app.route('/number_template/<int:path>', strict_slashes=False)
 def show_your_number_template(path):
-    if path.isdigit():
-        return render_template('5-number.html', number=path)
-    else:
-        abort(404)
+    """ print the path after /number_template/ if it is number """
+    return render_template('5-number.html', number=path)
 
 
 if __name__ == '__main__':
