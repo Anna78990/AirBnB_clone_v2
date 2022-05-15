7ba31cc0847ab1fddd75a8ddfc8b779a469c7c2e
Anna78990 <takasuanna1990@gmail.com>
2022-05-15 12:05:21 +0000

    0-setup_web_static.sh

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 from flask import Flask
+""" practice of flask """
+
 
 app = Flask(__name__)
 
+
+""" print hello world """
 @app.route('/', strict_slashes=False)
 def index():
-  return "Hello HBNB!" 
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="0.0.0.0") 
+    app.run(host="0.0.0.0")
