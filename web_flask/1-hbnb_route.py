057d64c397f9a4ad6b9794bd80c7742319796df6
Anna78990 <takasuanna1990@gmail.com>
2022-05-15 12:14:30 +0000

    modify 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,21 @@
+#!/usr/bin/python3
+""" practice of flask """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!" 
+    """ print hello world """
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ print HBNB """
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="0.0.0.0") 
+    app.run(host="0.0.0.0")
