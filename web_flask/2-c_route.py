5627b783ec8f9e8ac31cd1d3e647328ceb8b05e0
Anna78990 <takasuanna1990@gmail.com>
2022-05-15 12:18:05 +0000

    2-do_deploy_web_static.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,29 @@
+#!/usr/bin/python3
+""" practice of flask """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!" 
+    """ print hello world """
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ print HBNB """
     return "HBNB"
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/c/<path:path>', strict_slashes=False)
 def show_your_text(path):
+    """ print C is 'path' """
     path = path.replace('_', ' ')
     return 'C %s' % path
 
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="0.0.0.0") 
+    app.run(host="0.0.0.0")
