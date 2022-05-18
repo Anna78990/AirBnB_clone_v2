0b5741f468f3083a8809494973d4e73fa082972b
Anna78990 <takasuanna1990@gmail.com>
2022-05-18 21:27:23 +0000

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,17 +6,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def close_session(exception):
+    """Close session"""
+    storage.close()
+
 @app.route('/states_list', strict_slashes=False)
 def display_state():
     """ print state list """
     states = storage.all(State).values()
-    return render_template("7-states_list.html", id=states._id, name=states.name)
-
-@app.teardown_appcontext
-def close_session(exception):
-    """Close the SQLAlchemy session"""
-    storage.close()
+    return render_template("7-states_list.html", state=states)
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host="0.0.0.0")
