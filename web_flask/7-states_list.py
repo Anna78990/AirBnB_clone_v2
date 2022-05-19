705ace7581035ad2c39319cd1badf303b71d178b
Anna78990 <takasuanna1990@gmail.com>
2022-05-19 15:37:14 +0000

    state =

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,4 +18,4 @@ def display_state():
     return render_template("7-states_list.html", state=states)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0" port=5000)
